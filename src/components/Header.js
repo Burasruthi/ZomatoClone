251a0c58ba14eece937919640dec61c1576a7721
Burasruthi <burasruthi12@gmail.com>
2025-08-19 23:37:01 +0530

    version 3 done responsive for menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,28 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 const Header = () => {
+  
+  let hanburgEle =useRef()
+  let showMenu = ()=>{
+    hanburgEle.current.classList.toggle('active')
+    document.body.classList.add('active')
+  }
+  let removeMenu=()=>{
+    hanburgEle.current.classList.remove('active')
+  }
+  
 
   let ele=useRef();
 
+  let sigEle = useRef()
+ console.log(ele.current)
+ console.log(sigEle.current)
   let showLogin=()=>{
     let isActive=ele.current.classList.toggle('active')
     if(isActive){
         document.querySelector(".header").classList.add("dark");
         document.body.classList.add('darkBg')
+        
     }
     else{
        document.querySelector(".header").classList.remove("dark");
@@ -25,6 +39,25 @@ const Header = () => {
       ele.current.classList.remove('active');
       document.querySelector(".header").classList.remove("dark");
       document.body.classList.remove('darkBg')
+      
+  }
+
+
+let showSignUp = () => {
+  let isActive = sigEle.current.classList.toggle('active');
+  if (isActive) {
+    document.querySelector(".header").classList.add("dark");
+    document.body.classList.add("darkBg");
+  } else {
+    document.querySelector(".header").classList.remove("dark");
+    document.body.classList.remove("darkBg");
+  }
+};
+
+   let removeSignUp=()=>{
+      sigEle.current.classList.remove('active');
+      document.querySelector(".header").classList.remove("dark");
+      document.body.classList.remove('darkBg')
   }
   return (
     <>
@@ -32,12 +65,18 @@ const Header = () => {
             <img src="https://b.zmtcdn.com/web_assets/81f3ff974d82520780078ba1cfbd453a1583259680.png" alt="image"/>
             <h1>zomato</h1>
             <h4>Find the best restaurants, cafés <br></br><span>and bars in India</span></h4>
-            <div className='nav'>
+            <div className='nav' ref={hanburgEle}>
+              <h4>Zomato</h4>
               <a>Add restaurants</a>
               <a onClick={showLogin}>Log in</a>
-              <a>Sign up</a>
+              <a onClick={showSignUp}>Sign up</a>
+              <div className='wrong'>
+                  <p onClick={removeMenu}>&#10006;</p>
+              </div>
             </div>
-            <img style={{ width: "40px", height: "50px", cursor: "pointer"}} src="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg"/>
+            {/* <img style={{ width: "40px", height: "50px", cursor: "pointer"}} src="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg"/> */}
+            <div class="hamburger"><p onClick={showMenu}>&#9776;</p></div>
+
           
         </div>
         <div className='login' ref={ele}>
@@ -83,8 +122,36 @@ const Header = () => {
           </form>
           
         </div>
-        <div className='signup'>
-
+        <div  className='signup' ref={sigEle}>
+          <form>
+            <div className='signuptop'>
+                <h3>Sign Up</h3>
+                <p style={{margin: "23px", fontSize:"20px", cursor:"pointer"}} onClick={removeSignUp}>&#10006;</p>
+            </div>
+            <div className='signup-text'>
+                <input type="text" placeholder='Full Name'/><br></br>
+                <input type="text" placeholder='Email'/><br></br>
+                
+                
+            </div>
+            <div className='signup-checkbox'>
+                <input type='checkbox'/><p >I agree to Zomato's <span style={{color:"red"}}>Terms of Services, Privacy Policy </span>and <span style={{color:"red"}}>Content Polices</span> </p>
+            </div>
+            <div className='sigupdown1'>
+                <p>Create account</p><br></br>
+                <i>Or</i>
+                
+            </div>
+            <div className='signupdown2'>
+                <img style={{height:"20px", width:"20px"}} src="https://images.icon-icons.com/2699/PNG/512/google_logo_icon_169090.png"/>
+                <span style={{paddingLeft:"5px",paddingTop:"10px"}}>Sign in with Google</span>
+            </div>
+            <div className='sinupdown3'>
+                <hr></hr>
+                <p>Already have an account? <span style={{color:"red"}}>Log in</span> </p>
+            </div>
+          </form>
+          
         </div>
        
     </>
